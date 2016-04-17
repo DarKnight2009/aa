0c7da28b83bb8a3bdb80e73ecabf77bc73d54f65
xuan <464430440@qq.com>
2016-04-17 23:11:19 +0800

    mm

--- success.php
+++ success.php
@@ -3,18 +3,18 @@
 
 <body>
 	<?php
-$con = mysql_connect("lrahalbdqmxq.rs.sae.sina.com.cn：10106","xuan","xuan95shi02yu04");
+$con = mysql_connect("lrahalbdqmxq.rs.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
 if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+{
+die('Could not connect: ' . mysql_error());
+}
 
 // some code
-  mysql_select_db('qiangpiao',$con);
-  $sql="INSERT INTO qiang (name,IDinput,phonenumber)
+mysql_select_db("qiangpiao",$con);
+$sql="INSERT INTO qiang (name,IDinput,phonenumber)
 VALUES
 ('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
-mysql_query($sql，$con);
+mysql_query($sql);
 mysql_close($con);
 echo "<h1>大姨妈</h1>";
 ?>
