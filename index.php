0e57af17bbecca8d20e2c4aa4d574d4418bda268
xuan <464430440@qq.com>
2016-04-17 23:06:17 +0800

    mm

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 		$sql1="SELECT * FROM qiang";
 		$count1=mysql_num_rows(mysql_query($sql1)); 
 		$count1=300-$count1;
-		$str="<h2 text-align:center>"+"$count1"+"</h2>";
-		echo $str;
+		echo "<h2 text-align:center>".$count1."</h2>";
+		
 	?>
 	<form action="test.php" method="post" accept-charset="utf-8" class="former">
 		
