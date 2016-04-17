4de1912bc990277f0d315757404959fd33b82e34
xuan <464430440@qq.com>
2016-04-17 16:47:38 +0800

    m

--- test.php
+++ test.php
@@ -10,9 +10,9 @@ VALUES
 ('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
 mysql_query($sql,$con);
 mysql_close($con);
-echo "$_POST[name]";
-echo "$_POST[IDinput]";
-echo "$_POST[phonenumber]";
+echo "恭喜你抢票成功";
+
+
 ?> 
 
 </body>
