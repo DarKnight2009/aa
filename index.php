bde003f509ccde2d6193ad3269483b520f2ea734
xuan <464430440@qq.com>
2016-04-17 15:44:54 +0800

    m

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>抢票系统</title>
+		
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>抢票系统</h1>
+	<form action="success.php" method="post" accept-charset="utf-8" class="former">
+		
+			<label>姓名:</label>
+			<br><br>
+			<input type="text" name="name"  class="b">
+			<br><br>
+			<label>证件号:</label><br><br>
+			<input type="text" name="IDinput" class="b" placeholder="学生输入学生证号老师输入教师证号"><br><br>
+			<label >电话</label>
+			<br><br>
+			<input type="text" name="phonenumber" class="b">	
+			<br><br><br><br>	
+		
+		<input type="submit" name="ok" value="抢票" class="a" >
+	</form>
+	
+</body>
+</html>
---
+++ success.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	<?php
+$con = mysql_connect("lrahalbdqmxq.rs.sae.sina.com.cn：10106","xuan","xuan95shi02yu04");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+// some code
+  mysql_select_db('qiangpiao',$con);
+  $sql="INSERT INTO qiang (name,IDinput,phonenumber)
+VALUES
+('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
+mysql_query($sql，$con);
+mysql_close($con);
+echo "<h1>大姨妈</h1>";
+?>
+
+</body>
+</html>
+
+
