336c01c2e6522b213c4cbb816c86e43a1aa511e9
xuan <464430440@qq.com>
2016-04-17 16:41:14 +0800

    m

--- test.php
+++ test.php
@@ -6,7 +6,7 @@
 $con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
 mysql_select_db("qiangpiao",$con);
 $sql="insert into qiang(name.IDinput,phonenumber)
-values('$_POST[name]','$_POST[IDinput]',$_POST[phonenumber])";
+values('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
 mysql_query($sql,$con);
 mysql_close($con);
 echo "$_POST[name]";
