d80bb15baf25c9aa76011009a02a1d6c6fa09429
xuan <464430440@qq.com>
2016-04-17 16:36:15 +0800

    m

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php 
-$con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn","xuan","xuan95shi02yu04");
+$con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
 mysql_select_db("qiangpiao",$con);
 $sql="insert into qiang(name.IDinput,phonenumber)
 values('$_POST[name]','$_POST[IDinput]',$_POST[phonenumber])";
