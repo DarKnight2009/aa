902f58bb1c4e0526396e440da65a4a8435acbdfc
xuan <464430440@qq.com>
2016-04-18 23:17:51 +0800

    mm

--- test.php
+++ test.php
@@ -15,14 +15,15 @@ $sql1="SELECT * FROM qiang";
 $count=mysql_num_rows(mysql_query($sql1));
 $sql2="SELECT * FROM qiang WHERE phonenumber='$_POST[phonenumber]'";
 $sql3="SELECT * FROM qiang WHERE IDinput='$_POST[IDinput]'";
+echo "哒哒哒";
 if(mysql_num_rows(mysql_query($sql2))||mysql_num_rows(mysql_query($sql3)))
 {
-	echo "<div class="page">
-    		<div class="weui_msg">
-        	<div class="weui_icon_area"><i class="weui_icon_warn weui_icon_msg"></i></div>
-        	<div class="weui_text_area">
-            <h2 class="weui_msg_title">抢票失败</h2>
-            <p class="weui_msg_desc">一个人只能抢一张票哟！</p>
+	echo "<div class='page'>".
+    		"<div class='weui_msg'>".
+        	"<div class='weui_icon_area'>"."<i class='weui_icon_warn weui_icon_msg'></i></div>".
+        	"<div class='weui_text_area'>".
+            "<h2 class='weui_msg_title'>抢票失败</h2>".
+            "<p class='weui_msg_desc'>一个人只能抢一张票哟！</p>
         </div>";
 }
 else
