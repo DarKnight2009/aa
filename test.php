9cf6759a97c29947efe550d9be5f917d057f3e95
xuan <464430440@qq.com>
2016-04-18 15:59:18 +0800

    mm

--- test.php
+++ test.php
@@ -13,20 +13,29 @@ $con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn:10106","xuan","xuan95shi02y
 mysql_select_db("qiangpiao",$con);
 $sql1="SELECT * FROM qiang";
 $count=mysql_num_rows(mysql_query($sql1));
-if($count<100)
+$sql2="SELECT * FROM qiang WHERE phonenumber='$_POST[phonenumber]'";
+$sql3="SELECT * FROM qiang WHERE IDinput='$_POST[IDinput]'";
+if(mysql_query($sql2)&&mysql_query($sql3))
 {
-	$sql="INSERT INTO qiang (name,IDinput,phonenumber)
-	VALUES
-	('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
-	mysql_query($sql,$con);
-	echo "<h1>抢票成功！！！</h1>";
+	echo "<h1>请勿使用相同手机号或学生证号抢票!</h1>";
 }
 else
 {
-	echo "<h1>亲，你晚了一步票已经抢完了!</h1>";
-}
+	if($count<100)
+	{
+		$sql="INSERT INTO qiang (name,IDinput,phonenumber)
+		VALUES
+		('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
+		mysql_query($sql,$con);
+		echo "<h1>抢票成功！！！</h1>";
+	}
+	else
+	{
+		echo "<h1>亲，你晚了一步票已经抢完了!</h1>";
+	}
+}	
 mysql_close($con);
-echo "$count";
+
 
 
 ?>
