f6f50fdd325c5f1f45a4522e2041153a18915ff7
xuan <464430440@qq.com>
2016-04-18 16:43:42 +0800

    mm

--- index.php
+++ index.php
@@ -5,26 +5,41 @@
 	<title>抢票系统</title>
 		
 	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="dist/style/weui.css">
 	<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
 </head>
 <body>
 	<h1>抢票系统</h1>
-
+	<h2 id="title"></h2>
+	<br><br><br>
 	<form action="test.php" method="post" accept-charset="utf-8" class="former">
-			<h2 id="title"></h2>
-			<label>姓名:</label>
-			<br><br>
-			<input type="text" name="name"  class="b">
-			<br><br>
-			<label>证件号:</label><br><br>
-			<input type="text" name="IDinput" class="b" placeholder="学生输入学生证号老师输入教师证号"><br><br>
-			<label >电话</label>
-			<br><br>
-			<input type="text" name="phonenumber" class="b">	
-			<br><br><br><br>	
+
 		
-		<input type="submit" name="ok" value="抢票" class="a" id="sure" >
-	</form>
 	
+    <div class="weui_cells_title">表单</div>
+        <div class="weui_cells weui_cells_form">
+            <div class="weui_cell">
+                <div class="weui_cell_hd"><label class="weui_label">姓名  </label></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <input class="weui_input" type="text"  placeholder="请输入姓名"  name="name"/>
+                </div>
+            </div>
+          
+            <div class="weui_cell">
+                <div class="weui_cell_hd"><label class="weui_label">证件号  </label></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <input class="weui_input" type="text" name="IDinput" placeholder="学生输入学生证号教职工输入教职工号"/>
+                </div>
+            </div>
+            <div class="weui_cell">
+                <div class="weui_cell_hd"><label class="weui_label">电话  </label></div>
+                <div class="weui_cell_bd weui_cell_primary">
+                    <input class="weui_input" type="text" name="phonenumber" placeholder="请输入电话号码"/>
+                </div>
+            </div>
+         </div>
+     </div>  
+     <input type="submit" name="ok" value="抢票" class="a" id="sure" class="weui_btn">
+     </form>  
 </body>
 </html>
