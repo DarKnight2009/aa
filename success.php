ff6c248e7d41ff49f03687be0e267fc8999fa484
xuan <464430440@qq.com>
2016-04-18 15:43:12 +0800

    mm

--- success.php
+++ success.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
-<html>
 
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+	
+</head>
 <body>
 	<?php
-$con = mysql_connect("lrahalbdqmxq.rs.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
-if (!$con)
-{
-die('Could not connect: ' . mysql_error());
-}
-
-// some code
-mysql_select_db("qiangpiao",$con);
-$sql="INSERT INTO qiang (name,IDinput,phonenumber)
-VALUES
-('$_POST[name]','$_POST[IDinput]','$_POST[phonenumber]')";
-mysql_query($sql);
-mysql_close($con);
-echo "<h1>大姨妈</h1>";
-?>
+		$con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
+		mysql_select_db("qiangpiao",$con);
+		$sql1="SELECT * FROM qiang";
+		$count1=mysql_num_rows(mysql_query($sql1)); 
+		$count1=300-$count1;
+		echo "<h2>剩余票数为："."$count1"."</h2>";
+		mysql_close($con);
+		
+	?>
 
 </body>
 </html>
