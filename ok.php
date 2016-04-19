6b0492eea933c5270222fe744c13413b38d847aa
xuan <464430440@qq.com>
2016-04-19 16:44:09 +0800

    mm

--- ok.php
+++ ok.php
@@ -6,9 +6,10 @@
 </head>
 <body>
 	<?php
+		$q=$_GET['id'];
 		$con=mysql_connect("lrahalbdqmxq.rds.sae.sina.com.cn:10106","xuan","xuan95shi02yu04");
 		mysql_select_db("qiangpiao",$con);
-		$sql="DELETE FROM qiang WHERE IDinput='$_GET[id]'";
+		$sql="DELETE FROM qiang WHERE IDinput=q";
 		mysql_query($sql);
 		mysql_close($con);
 		
