cd4d142be0c5ae14daa6be85feedfe024a26b8d2
xuan <464430440@qq.com>
2016-05-06 22:05:00 +0800

    mm

--- index.php
+++ index.php
@@ -7,12 +7,23 @@
 		
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="dist/style/weui.css">
-	<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
+	<!--此处注释
+	<script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>-->
 </head>
 <body>
 	<h1>抢票系统</h1>
+	 <div class='page'>
+    		<div class='weui_msg'>
+        	<div class='weui_icon_area'>
+        	<i class='weui_icon_warn weui_icon_msg'></i></div>
+        	<div class='weui_text_area'>
+            <h2 class='weui_msg_title'>很遗憾！</h2>
+            <p class='weui_msg_desc'>本次演出抢票活动已经结束</p>
+        </div>
+	<!--注释掉
 	<h2 id="title"></h2>
 	<br><br><br>
+	
 	<form action="test.php" method="post" accept-charset="utf-8" style={{cursor:'pointer'}} >
 
 		
@@ -85,5 +96,6 @@
      </form>  
     
 </div>
+-->
 </body>
 </html>
