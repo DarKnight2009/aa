49bb88f2d2d8ee645413dbb10908a2041a5ed54f
xuan <464430440@qq.com>
2016-05-14 13:50:43 +0800

    mm

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>抢票成功</title>
+	<title>抢票结果</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">	
 	<link rel="stylesheet" href="dist/style/weui.css">
 </head>
